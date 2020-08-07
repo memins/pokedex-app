b27b3dd927469034e72f93da26df32dd7b39ac47
Muhammed Sahin <muhammed.sahin@acp.at>
2020-08-07 19:32:26 +0200

    adding the header and sources component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SourcesComponent } from './sources/sources.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'sources', component: SourcesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SourcesComponent } from './sources/sources.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SourcesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
